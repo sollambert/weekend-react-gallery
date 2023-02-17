9a7524d6be6701f046c96b080acf3638f4ac1f26
Solomon Lambert <sol.lambert@gmail.com>
2023-02-17 17:05:05 -0600

    add db and update get request

--- database.sql
+++ database.sql
@@ -1,1 +1,14 @@
-create table ''
+create table "images" (
+    "id" SERIAL INT,
+    "path" VARCHAR (80),
+    "description" TEXT,
+    "likes" INT default 0
+);
+
+INSERT INTO "images" ("path", "description")
+VALUES('images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park.'),
+    ('images/club-penguin-breakdance.gif', 'The best god damn thing in the world.'),
+    ('images/amethyst-cat.png', 'meow'),
+    ('images/mazda-protege.jpeg', 'shit car'),
+    ('images/Orc-5e.webp', 'big orc boy'),
+    ('images/photo_of_me.jpg', 'it me');
